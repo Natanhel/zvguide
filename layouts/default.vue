df2d4ce39ed13caed4e57ed541f95e192df95016
Natanhel Poliszuk <natanhelp@gmail.com>
2020-03-31 18:53:56 +0300

    Can't select title anymore

--- layouts/default.vue
+++ layouts/default.vue
@@ -128,8 +128,24 @@ export default {
 </script>
 
 <style>
-.closeBtn{
+.closeBtn {
   padding: 1em;
   margin: 1em;
+}
+v-navigation-drawer,
+p,
+div,
+v-card-title,
+v-btn,
+h1,
+h2,
+h3,
+h4,
+h5 {
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* Internet Explorer */
+  -khtml-user-select: none; /* KHTML browsers (e.g. Konqueror) */
+  -webkit-user-select: none; /* Chrome, Safari, and Opera */
+  -webkit-touch-callout: none; /* Disable Android and iOS callouts*/
 }
 </style>
